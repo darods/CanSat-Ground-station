f9ef51bee3f86b71d5e051672bb0bae647b09fa8
NASA <daniel_arod@protonmail.com>
2019-08-11 17:23:29 -0500

    escogido y modificado mejor ejemplo de interfaz

--- Interfaz.py
+++ Interfaz.py
@@ -1,59 +1,86 @@
-from PyQt5 import QtGui  # (the example applies equally well to PySide)
-import pyqtgraph as pg
+"""
+Demonstrate the use of layouts to control placement of multiple plots / views /
+labels
+
+
+"""
+
 from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
 import numpy as np
-# Always start by initializing Qt (only once per application)
+
 app = QtGui.QApplication([])
+view = pg.GraphicsView()
+l = pg.GraphicsLayout(border=(100, 100, 100))
+view.setCentralItem(l)
+view.show()
+view.setWindowTitle('pyqtgraph example: GraphicsLayout')
+view.resize(800, 600)
 
-# Define a top-level widget to hold everything
-w = QtGui.QWidget()
+# Title at top
+text = """
+This puto example demonstrates the use of GraphicsLayout to arrange items in a grid.<br>
+The items added to the layout must be subclasses of QGraphicsWidget (this includes <br>
+PlotItem, ViewBox, LabelItem, and GrphicsLayout itself).
+"""
+l.addLabel(text, col=1, colspan=4)
+l.nextRow()
 
-# Create some widgets to be placed inside
-btn = QtGui.QPushButton('press me')
-text = QtGui.QLineEdit('enter text')
-listw = QtGui.QListWidget()
-plot = pg.PlotWidget(name='plot 1')
-plot2 = pg.PlotWidget(name='plot 2')
-plot3 = pg.PlotWidget(name='plot 3')
-plot4 = pg.PlotWidget(name='plot 3')
+# Put vertical label on left side
+l.addLabel('Long Vertical Label', angle=-90, rowspan=3)
 
-curve = plot4.plot(pen='r')
-data = np.random.normal(size=(10, 1000))
-ptr = 0
-
-
-def update():
-    global curve, data, ptr, p6
-    curve.setData(data[ptr % 10])
-    if ptr == 0:
-        # stop auto-scaling after the first data set is plotted
-        plot4.enableAutoRange('xy', False)
-    ptr += 1
-
-
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(50)
+# Add 3 plots into the first row (automatic position)
+p1 = l.addPlot(title="Plot 1")
+p2 = l.addPlot(title="Plot 2")
+vb = l.addViewBox(lockAspect=True)
+img = pg.ImageItem(np.random.normal(size=(100, 100)))
+vb.addItem(img)
+vb.autoRange()
 
 
-plot.setLabel('left', 'Value', units='V')
+# Add a sub-layout into the second row (automatic position)
+# The added item should avoid the first column, which is already filled
+l.nextRow()
+l2 = l.addLayout(colspan=1, border=(50, 0, 0))
+l2.setContentsMargins(10, 10, 10, 10)
+l2.addLabel(
+    "Sub-layout: this layout demonstrates the use of shared axes and axis labels", colspan=3)
+l2.nextRow()
+l2.addLabel('Vertical Axis Label', angle=-90, rowspan=2)
+p21 = l2.addPlot()
+p22 = l2.addPlot()
+l2.nextRow()
+p23 = l2.addPlot()
+p24 = l2.addPlot()
+l2.nextRow()
+l2.addLabel("HorizontalAxisLabel", col=1, colspan=2)
 
-# Create a grid layout to manage the widgets size and position
-layout = QtGui.QGridLayout()
-w.setLayout(layout)
-
-# Add widgets to the layout in their proper positions
-layout.addWidget(btn, 0, 0)   # button goes in upper-left
-layout.addWidget(text, 1, 0)   # text edit goes in middle-left
-layout.addWidget(listw, 2, 0)  # list widget goes in bottom-left
-layout.addWidget(plot, 0, 1)  # plot goes on right side, spanning 3 rows
-layout.addWidget(plot2, 0, 3)  # plot goes on right side, spanning 3 rows
-layout.addWidget(plot3, 2, 1)  # plot goes on right side, spanning 3 rows
-layout.addWidget(plot4, 2, 3)  # plot goes on right side, spanning 3 rows
+# hide axes on some plots
+p21.hideAxis('bottom')
+p22.hideAxis('bottom')
+p22.hideAxis('left')
+p24.hideAxis('left')
+p21.hideButtons()
+p22.hideButtons()
+p23.hideButtons()
+p24.hideButtons()
 
 
-# Display the widget as a new window
-w.show()
+# Add 2 more plots into the third row (manual position)
+p4 = l.addPlot(row=3, col=1)
+p5 = l.addPlot(row=3, col=2, colspan=2)
 
-# Start the Qt event loop
-app.exec_()
+# show some content in the plots
+p1.plot([1, 3, 2, 4, 3, 5])
+p2.plot([1, 3, 2, 4, 3, 5])
+p4.plot([1, 3, 2, 4, 3, 5])
+p5.plot([1, 3, 2, 4, 3, 5])
+
+
+p6 = l.addPlot(row=2, col=2, colspan=2)
+
+# Start Qt event loop unless running in interactive mode.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
