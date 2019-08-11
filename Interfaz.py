25df3e0a768206d36128cd67b0b2a6d00001eaed
NASA <daniel_arod@protonmail.com>
2019-08-11 17:56:21 -0500

    ajustada la interfaz al diseño

--- Interfaz.py
+++ Interfaz.py
@@ -1,10 +1,3 @@
-"""
-Demonstrate the use of layouts to control placement of multiple plots / views /
-labels
-
-
-"""
-
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
@@ -27,48 +20,52 @@ l.addLabel(text, col=1, colspan=4)
 l.nextRow()
 
 # Put vertical label on left side
-l.addLabel('Long Vertical Label', angle=-90, rowspan=3)
+l.addLabel('RITA - Universidad Distrital', angle=-90, rowspan=3)
 
 # Add 3 plots into the first row (automatic position)
-p1 = l.addPlot(title="Plot 1")
-p2 = l.addPlot(title="Plot 2")
-vb = l.addViewBox(lockAspect=True)
+l1 = l.addLayout(colspan=1, border=(50, 0, 0))
+l1.setContentsMargins(10, 10, 10, 10)
+l1.addLabel(
+    "Sub-layout: this layout demonstrates the use of shared axes and axis labels", colspan=3)
+l1.nextRow()
+p1 = l1.addPlot(title="Plot 1")
+
+l2 = l.addLayout(colspan=1, border=(50, 0, 0))
+l2.setContentsMargins(10, 10, 10, 10)
+l2.addLabel(
+    "Sub-layout: this layout demonstrates the use of shared axes and axis labels", colspan=2)
+l2.nextRow()
+p2 = l2.addPlot(title="Plot 2", rowspan=2)
+vb = l2.addViewBox(lockAspect=True)
 img = pg.ImageItem(np.random.normal(size=(100, 100)))
 vb.addItem(img)
 vb.autoRange()
-
+l2.nextRow()
+p6 = l2.addPlot()
 
 # Add a sub-layout into the second row (automatic position)
 # The added item should avoid the first column, which is already filled
 l.nextRow()
-l2 = l.addLayout(colspan=1, border=(50, 0, 0))
-l2.setContentsMargins(10, 10, 10, 10)
-l2.addLabel(
+l3 = l.addLayout(colspan=1, border=(50, 0, 0))
+l3.setContentsMargins(10, 10, 10, 10)
+l3.addLabel(
     "Sub-layout: this layout demonstrates the use of shared axes and axis labels", colspan=3)
-l2.nextRow()
-l2.addLabel('Vertical Axis Label', angle=-90, rowspan=2)
-p21 = l2.addPlot()
-p22 = l2.addPlot()
-l2.nextRow()
-p23 = l2.addPlot()
-p24 = l2.addPlot()
-l2.nextRow()
-l2.addLabel("HorizontalAxisLabel", col=1, colspan=2)
-
-# hide axes on some plots
-p21.hideAxis('bottom')
-p22.hideAxis('bottom')
-p22.hideAxis('left')
-p24.hideAxis('left')
-p21.hideButtons()
-p22.hideButtons()
-p23.hideButtons()
-p24.hideButtons()
+l3.nextRow()
+l3.addLabel('Vertical Axis Label', angle=-90, rowspan=2)
+p21 = l3.addPlot()
+p22 = l3.addPlot()
 
 
-# Add 2 more plots into the third row (manual position)
-p4 = l.addPlot(row=3, col=1)
-p5 = l.addPlot(row=3, col=2, colspan=2)
+l4 = l.addLayout(colspan=1, border=(50, 0, 0))
+l4.setContentsMargins(10, 10, 10, 10)
+l4.addLabel(
+    "Sub-layout: this layout demonstrates the use of shared axes and axis labels", colspan=2)
+l4.nextRow()
+p4 = l4.addPlot(title="Plot 4", rowspan=2)
+p5 = l4.addPlot(title="Plot 5")
+
+l4.nextRow()
+p6 = l4.addPlot()
 
 # show some content in the plots
 p1.plot([1, 3, 2, 4, 3, 5])
@@ -76,8 +73,6 @@ p2.plot([1, 3, 2, 4, 3, 5])
 p4.plot([1, 3, 2, 4, 3, 5])
 p5.plot([1, 3, 2, 4, 3, 5])
 
-
-p6 = l.addPlot(row=2, col=2, colspan=2)
 
 # Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
