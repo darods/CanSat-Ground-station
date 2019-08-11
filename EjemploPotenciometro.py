7b2a43e0bae21fd7e61c69d482b105ead42a03f2
NASA <daniel_arod@protonmail.com>
2019-08-10 22:13:37 -0500

    grafica el dato del tiempo

--- EjemploPotenciometro.py
+++ EjemploPotenciometro.py
@@ -7,11 +7,11 @@ import numpy as np
 import serial
 
 # Create object serial port
-portName = '/dev/ttyUSB0'                     # replace this port name by yours!
+portName = '/dev/ttyACM0'
 baudrate = 9600
 ser = serial.Serial(portName, baudrate)
 
-### START QtApp #####
+#  START QtApp #
 # you MUST do this once (initialize things)
 app = QtGui.QApplication([])
 ####################
@@ -26,27 +26,33 @@ windowWidth = 500                    # width of the window displaying the curve
 Xm = linspace(0, 0, windowWidth)
 ptr = -windowWidth                      # set first x position
 
-# Realtime data plot. Each time this function is called, the data display is updated
+# Realtime data plot. Each time this function is called,
+# the data display is updated
 
 
 def update():
     global curve, ptr, Xm
     # shift data in the temporal mean 1 sample left
     Xm[:-1] = Xm[1:]
-    value = ser.readline()                # read line (single value) from the serial port
+    value = ser.readline()  # read line (single value) from the serial port
+    decoded_bytes = str(value[0:len(value) - 2].decode("utf-8"))
+    print(decoded_bytes)
+
+    valor = decoded_bytes.split(",")
+
     # vector containing the instantaneous values
-    Xm[-1] = float(value)
-    ptr += 1                              # update x position for displaying the curve
+    Xm[-1] = float(valor[0])
+    ptr += 1  # update x position for displaying the curve
     curve.setData(Xm)                     # set the curve with this data
     curve.setPos(ptr, 0)                   # set x position in the graph to 0
     QtGui.QApplication.processEvents()    # you MUST process the plot now
 
 
-### MAIN PROGRAM #####
+# MAIN PROGRAM #
 # this is a brutal infinite loop calling your realtime data plot
 while True:
     update()
 
-### END QtApp ####
+# END QtApp #
 app.exec_()  # you MUST put this at the end
 ##################
